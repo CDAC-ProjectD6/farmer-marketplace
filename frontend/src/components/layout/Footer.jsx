7345d224d5d79e5d564611b671ef39feed30a481
sdixit70 <shyamlidixit1999@gmail.com>
2026-07-20 00:05:04 +0530

    feat: add navbar and footer layout components

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer(){
+    return(
+        <footer>
+            <p>
+                © 2026 Farmer Marketplace
+                  </p>
+                
+                <p>Privacy Policy</p>
+                <p>Terms & Conditions</p>
+                <p>Contact</p>
+          
+        </footer>
+    );
+}
+
+export default Footer;
