a9b797250fffcb7d874ea4da65a4a09f20aaaa7b
Prajakta2909 <prajaktajagdale75@gmail.com>
2026-07-27 13:28:08 +0530

    Add user registration and login models

---
+++ backend/src/main/java/com/cdac/farmermarketplace/entity/Role.java
@@ -0,0 +1,8 @@
+package com.cdac.farmermarketplace.entity;
+
+public enum Role {
+	
+	FARMER,
+    CONSUMER,
+    ADMIN
+}
---
+++ backend/src/main/java/com/cdac/farmermarketplace/entity/User.java
@@ -0,0 +1,39 @@
+package com.cdac.farmermarketplace.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String mobile;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean active = true;
+}
